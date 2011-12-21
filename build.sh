67597a68e662c96e6b45b37748fadd6adad2fa8f
Kelly Brown <thebuzzsaw@gmail.com>
2011-12-20 17:58:01 -0700

    clean all the things

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ./source/
-mkdir build
-cd ./build
-cmake ../
-make 
-cd ../../
-./source/build/zero2d
--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,11 @@
 
 int main(int argc, char** argv)
 {
-    CGE::Engine e;
+    CGE::Engine::Settings s;
+    s.windowTitle = "Zero2D";
+
+    CGE::Engine e(s);
+
     CGE::Module m;
 
     e.run(m);
