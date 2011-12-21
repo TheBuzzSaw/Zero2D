e3ff81e11e5ce6f4f9c9098ad75649cdb38afe0c
Kelly Brown <thebuzzsaw@gmail.com>
2011-12-20 17:53:37 -0700

    a new beginning

--- source/CGE/Debug.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#ifdef NDEBUG
-#define LDB
-#else
-#define LDB std::cout << __FILE__ << " : " << __LINE__ << std::endl
-#endif
---
+++ source/main.cpp
@@ -0,0 +1,10 @@
+#include <CGE/Engine.h>
+
+int main(int argc, char** argv)
+{
+    CGE::Engine e;
+    CGE::Module m;
+
+    e.run(m);
+    return 0;
+}
